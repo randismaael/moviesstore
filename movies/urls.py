ead8bfcb431c80903f323bcde7a658eb21fbd3d7
randismaael <rismaael3@gatech.edu>
2025-10-19 02:23:54 -0400

    Fixed the reviews button

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = "movies"
-
 urlpatterns = [
     path("", views.index, name="movies.index"),
     path("<int:id>/", views.show, name="movies.show"),
-    path("<int:id>/rate/", views.rate_movie, name="movies.rate"),  # NEW
+    path("<int:id>/rate/", views.rate_movie, name="movies.rate"),
     path("<int:id>/reviews/create/", views.create_review, name="movies.create_review"),
     path(
         "<int:id>/reviews/<int:review_id>/edit/",
